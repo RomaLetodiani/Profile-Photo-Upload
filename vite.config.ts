fa3acb48d61faaab95f95880fdc8b7fb0aa0f264
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-02 17:24:19 +0400

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME',
+  base: '/Profile-Photo',
   plugins: [react()],
 });
